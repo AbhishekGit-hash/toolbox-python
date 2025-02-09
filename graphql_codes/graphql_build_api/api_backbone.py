831cf306a65061ae34eebf51db329d58257a7301
abhishek.chowdhury <abhishekchowdhury963@gmail.com>
2025-02-09 17:41:55 +0530

    Added API Backbone

---
+++ graphql_codes/graphql_build_api/api_backbone.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from starlette_graphene3 import GraphQLApp
+from graphene import (
+    Schema,
+    ObjectType,
+    String
+)
+
+
+class Query(ObjectType):
+
+    hello = String(name = String(default_value = "graphql"))
+
+    @staticmethod
+    def resolve_hello(root, info, name):
+        return f"Hello {name}"
+    
+schema = Schema(query=Query)
+
+app = FastAPI()
+
+app.mount("/graphql", GraphQLApp(schema=schema))
