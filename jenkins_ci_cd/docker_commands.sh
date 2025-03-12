4e73686139bc0b6067b38788272a03783ce69062
abhishek.chowdhury <abhishekchowdhury963@gmail.com>
2025-03-12 11:46:36 +0530

    Added some Jenkins commands

--- jenkins_ci_cd/docker_commands.sh
+++ jenkins_ci_cd/docker_commands.sh
@@ -8,3 +8,8 @@ docker images -f "dangling=true"
 # Run Jenkins in detached mode (-d) with port mapping (-p) so you can access it via the browser
 docker run -d --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts-jdk17
 
+# To get initial password
+docker exec my-jenkins cat /var/jenkins_home/secrets/initialAdminPassword
+
+# To get list of running containers
+docker ps -a
