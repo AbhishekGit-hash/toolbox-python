361ba49b720c3f911ed9df03bc4066598bd86797
abhishek.chowdhury <abhishekchowdhury963@gmail.com>
2025-03-12 10:56:21 +0530

    Added Jenkins CI/CD folder

---
+++ jenkins_ci_cd/docker_commands.sh
@@ -0,0 +1,10 @@
+
+# To list all Docker images on your system
+docker image ls
+
+# If you want to see dangling images (untagged images that take up space), run
+docker images -f "dangling=true"
+
+# Run Jenkins in detached mode (-d) with port mapping (-p) so you can access it via the browser
+docker run -d --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins:lts-jdk17
+
